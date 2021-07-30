303d67dcf30b6cc5e12b8895b48ac8e77b04bd39
Jian-G <541832360@qq.com>
2021-07-30 13:35:59 +0800

    fix detect bug

--- backend/model_transmit/load_model.py
+++ backend/model_transmit/load_model.py
@@ -1,9 +1,19 @@
-from sys import prefix
 import paddle
 import glob
 import numpy as np
 import time
-def edge_load_model(path_prefix):
+import cv2
+import paddle.vision.transforms as T
+
+def image_preprocess(img):
+    image = cv2.imread(img)
+    trans = T.Compose([T.Transpose(), T.Normalize((0.4914, 0.4822, 0.4465),(0.2470, 0.2435, 0.2616))])
+    image = trans(image)
+    image = np.expand_dims(image,0)
+    image = np.array(image, dtype=np.float32)
+    return image
+
+def edge_load_model(path_prefix,img):
     paddle.enable_static()
     startup_prog = paddle.static.default_startup_program()
     start_time = time.time()
@@ -15,9 +25,10 @@ def edge_load_model(path_prefix):
 
     [inference_program, feed_target_names, fetch_targets] = (
         paddle.static.load_inference_model(path_prefix, exe))
-    tensor_img = np.array(np.random.random((1, 3, 32, 32)), dtype=np.float32)
+    # tensor_img = np.array(np.random.random((1, 3, 32, 32)), dtype=np.float32)
+    # print(tensor_img)
     results = exe.run(inference_program,
-              feed={feed_target_names[0]: tensor_img},
+              feed={feed_target_names[0]: image_preprocess(img)},
               fetch_list=fetch_targets)
     end_time = time.time()
     return np.array(results[0]), round(end_time - start_time, 3)
@@ -43,8 +54,12 @@ def cloud_load_tensor(path_prefix, tensor):
     return [result[i].index(max(result[i])) for i in range(len(result))], round(end_time - start_time, 3)
 
 if __name__ == "__main__":
-    tensor,edge_infer_time = edge_load_model(path_prefix="../data/send/model/client_infer_resnet18_cifar10")
-    result,cloud_infer_time = cloud_load_tensor(path_prefix="../data/send/model/server_infer_resnet18_cifar10",tensor=tensor)
+    tensor,edge_infer_time = edge_load_model(
+        path_prefix="../data/send/model/client_infer_resnet18_cifar10",
+        img="../data/test/5/5_12.png")
+
+    result,cloud_infer_time = cloud_load_tensor(
+        path_prefix="../data/send/model/server_infer_resnet18_cifar10",tensor=tensor)
     print(result)
 
 
