d015c2ff2fcf614e8aedbd6fdcea0204825380d3
RoyXinyun <zhangxinyun99@163.com>
2021-08-04 20:05:52 +0800

    add database interface

--- frontend/flask/app.py
+++ frontend/flask/app.py
@@ -1,9 +1,17 @@
 from flask import *
 from flask_cors import *
 from test_model import infer
-
+from db_op import *
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:192223@127.0.0.1:3308/paddle'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# 查询时会显示原始SQL语句
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
 CORS(app, supports_credentials=True)
 
 @app.after_request
@@ -27,15 +35,22 @@ def hello():
 @app.route('/cut',methods=['POST'])
 def cut():
     print(request.data)
-    #模型切割模块....
-    return jsonify({'result':'success'})
+    model_divide_id = add_restriction(eval(request.data))
 
+    #模型切割模块....
+    edge_model,cloud_model,edge_cloud_model = add_submodel(model_divide_id)
+    edge = {'flops':edge_model.flops,'params':edge_model.params}
+    cloud = {'flops':cloud_model.flops,'params':cloud_model.params}
+    edge_cloud = {'flops':edge_cloud_model.flops,'params':edge_cloud_model.params}
+    models = {'edge':edge,'cloud':cloud,'edge_cloud':edge_cloud}
+    return jsonify({'result':'success','models':models})
 
 #图像测试文件上传
 @app.route('/image_upload',methods=['POST'])
 def image_upload():
     file = request.files.get('file')
-    directory = 'static/images/'
+    #directory = 'static/images/'
+    directory = '../../backend/data/test'
     file_path = directory + file.filename
     file.save(file_path)
     result = infer(file_path)
---
+++ frontend/flask/db_model.py
@@ -0,0 +1,58 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:192223@127.0.0.1:3308/paddle'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# 查询时会显示原始SQL语句
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Model(db.Model):
+    #表名
+    __tablename__ = 'model'
+    #列对象
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(255))
+
+class Restriction(db.Model):
+    __tablename__ = 'restriction'
+    id = db.Column(db.Integer, primary_key=True)
+    delay = db.Column(db.Float)
+    bandwidth = db.Column(db.Float)
+    cloud_computation = db.Column(db.Float)
+    edge_computation = db.Column(db.Float)
+
+class Submodel(db.Model):
+    __tablename__ = 'submodel'
+    id = db.Column(db.Integer, primary_key=True)
+    flops = db.Column(db.Float)
+    params = db.Column(db.Float)
+    infer_time = db.Column(db.Float)#推理时间（云-边模型：云边协同比）
+    type = db.Column(db.Integer)#模型类型 0：云-边模型  1：云模型  2：边模型
+    model_divide_id = db.Column(db.Integer)
+
+
+class ModelDivide(db.Model):
+    __tablename__ = 'model_divide'
+    id = db.Column(db.Integer, primary_key=True)
+    model_id = db.Column(db.Integer)
+    restric_id = db.Column(db.Integer)
+
+
+class System(db.Model):
+    __tablename__ = 'system'
+    id = db.Column(db.Integer, primary_key=True)
+    tensorsize = db.Column(db.Float)#传输数据量
+    transmit_time = db.Column(db.Float)#传输时间
+    time = db.Column(db.Float)
+    accuracy = db.Column(db.Float)
+    throughput = db.Column(db.Float)
+    model_divide_id = db.Column(db.Integer)
+
+if __name__ == '__main__':
+    db.drop_all()
+    db.create_all()
+
+
